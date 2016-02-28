6ba1934bda5352c3413adf892a46652f1161ad55
Bernhard Walle <bernhard@bwalle.de>
2016-02-28 10:40:31 +0100

    jahreskalender: Re-indent
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- jahreskalender/jahreskalender.pl
+++ jahreskalender/jahreskalender.pl
@@ -15,8 +15,8 @@
 
 # Module
 use Date::Calc ("Days_in_Month", "Delta_Days", "Easter_Sunday", "Add_Delta_Days",
-	"Week_Number", "Day_of_Week", "Month_to_Text", "Day_of_Week_Abbreviation",
-	"Decode_Language", "Language");
+    "Week_Number", "Day_of_Week", "Month_to_Text", "Day_of_Week_Abbreviation",
+    "Decode_Language", "Language");
 use PostScript::Simple;
 use sigtrap qw(die INT QUIT);
 use Getopt::Std;
@@ -29,7 +29,7 @@ getopts('o:y:t:hvnT:');
 
 if ($opt_h)
 {
-	print <<'EOF';
+    print <<'EOF';
     
 Aufruf: jahreskalender.pl [-o Datei] [-y Jahr] [-h]
 --------------------------------------------------------------------------------
@@ -74,14 +74,14 @@ EOF
 }
 
 END {
-	unlink("/tmp/kalenderdruck-$$.ps");
+    unlink("/tmp/kalenderdruck-$$.ps");
 }
 
 # Version ausgeben
 if ($opt_v) 
 {
-	print "Jahreskalender.pl, Version $VERSION\n";
-	exit 0;
+    print "Jahreskalender.pl, Version $VERSION\n";
+    exit 0;
 }
 
 # Welches Jahr?
@@ -104,183 +104,183 @@ Language(Decode_Language($LANG));
 $TITLE = $opt_T || "";
 
 $p = PostScript::Simple->new(landscape => 1,
-							units => 'mm',
-							papersize => 'A4',
-							colour => 1,
-							page => 1,
-							reencode => 'ISOLatin1Encoding',
-							eps => 0,
-						);
+                            units => 'mm',
+                            papersize => 'A4',
+                            colour => 1,
+                            page => 1,
+                            reencode => 'ISOLatin1Encoding',
+                            eps => 0,
+                        );
 
 $monat = 1;
 
 if ($TITLE)
 {
-	$TOP = 170;
-	$FONTSIZE = 8;
+    $TOP = 170;
+    $FONTSIZE = 8;
 }
 else
 {
-	$TOP = 190;
-	$FONTSIZE = 9;
+    $TOP = 190;
+    $FONTSIZE = 9;
 }
-						
+
 foreach $page (1, 2) 
 {
-	$p->newpage($page);
-	
-	$p->setfont('Helvetica-Bold-iso', 200);
-	$p->setcolour((0xE9, 0xE9, 0xE9));
-	$p->text(70, 80, $YEAR);
-	$p->setcolour("black");
-	$p->setlinewidth(0.7);
+    $p->newpage($page);
 
-	# Großer Titel
-	if ($TITLE)
-	{
-		$converter = Text::Iconv->new("utf-8", "iso8859-1");
-		$title_latin1 = $converter->convert($TITLE);
+    $p->setfont('Helvetica-Bold-iso', 200);
+    $p->setcolour((0xE9, 0xE9, 0xE9));
+    $p->text(70, 80, $YEAR);
+    $p->setcolour("black");
+    $p->setlinewidth(0.7);
 
-		$p->box(20, $TOP+20, 277, $TOP);
-		$p->setfont('Times-Bold-iso', 30);
-		$p->text(27, ($TOP+20)-14, $title_latin1);
-
-	}
-
-	# Rahmen und Titel
-	$p->box(20, 20, 277, $TOP);
-
-	# vertikale Linien
-	for ($i = 1; $i <= 5; $i++) 
+    # Großer Titel
+    if ($TITLE)
     {
-		$p->line((257/6*$i + 20), 20, (257/6*$i + 20), $TOP);
-	}
+        $converter = Text::Iconv->new("utf-8", "iso8859-1");
+        $title_latin1 = $converter->convert($TITLE);
 
-	$p->line(20, ($TOP-7), 277, ($TOP-7));
-	
-	$p->setfont('Times-Bold-iso', 12);
-	for ($monat = 1+($page-1)*6; $monat <= 6*$page; $monat++) 
+        $p->box(20, $TOP+20, 277, $TOP);
+        $p->setfont('Times-Bold-iso', 30);
+        $p->text(27, ($TOP+20)-14, $title_latin1);
+
+    }
+
+    # Rahmen und Titel
+    $p->box(20, 20, 277, $TOP);
+
+    # vertikale Linien
+    for ($i = 1; $i <= 5; $i++) 
+    {
+        $p->line((257/6*$i + 20), 20, (257/6*$i + 20), $TOP);
+    }
+
+    $p->line(20, ($TOP-7), 277, ($TOP-7));
+
+    $p->setfont('Times-Bold-iso', 12);
+    for ($monat = 1+($page-1)*6; $monat <= 6*$page; $monat++) 
     {	
-		$p->text(257/6*($monat-6*($page-1)-1)+23, $TOP-5,
-			Month_to_Text($monat));
-	}
+        $p->text(257/6*($monat-6*($page-1)-1)+23, $TOP-5,
+            Month_to_Text($monat));
+    }
 
-	$p->setlinewidth(0.1);
-	# vertikale Linien
-	for ($i = 0; $i <= 5; $i++) 
+    $p->setlinewidth(0.1);
+    # vertikale Linien
+    for ($i = 0; $i <= 5; $i++) 
     {
-		$p->line((257/6*$i + 31), 20, (257/6*$i + 31), $TOP-7);
-	}
-	
-	# horizontale Linien
-	for ($i = 1; $i < 31; $i ++) 
-    {
-		$p->line(20, (($TOP-7)-($TOP-7-20)/31*$i), 277, (($TOP-7)-($TOP-7-20)/31*$i));
-	}
+        $p->line((257/6*$i + 31), 20, (257/6*$i + 31), $TOP-7);
+    }
 
-	# Text einfügen
-	for ($monat = 1+($page-1)*6; $monat <= 6*$page; $monat++) 
+    # horizontale Linien
+    for ($i = 1; $i < 31; $i ++) 
+    {
+        $p->line(20, (($TOP-7)-($TOP-7-20)/31*$i), 277, (($TOP-7)-($TOP-7-20)/31*$i));
+    }
+
+    # Text einfügen
+    for ($monat = 1+($page-1)*6; $monat <= 6*$page; $monat++) 
     {	
-		for ($tag = 1; $tag <= 31; $tag++) 
+        for ($tag = 1; $tag <= 31; $tag++) 
         {
-			if ($tag < 10) 
+            if ($tag < 10) 
             {
-				$zusatz = 1.75;
-			}
+                $zusatz = 1.75;
+            }
             else 
             {
-				$zusatz = 0;
-			}
+                $zusatz = 0;
+            }
 
-			if ($tag <= Days_in_Month($YEAR, $monat)) 
+            if ($tag <= Days_in_Month($YEAR, $monat)) 
             {
-				$p->setfont('Helvetica-iso', $FONTSIZE);
+                $p->setfont('Helvetica-iso', $FONTSIZE);
 
-				
-				# Wochennummer am Monatsanfang
-				if ($tag == 1 and Day_of_Week($YEAR, $monat, $tag) == 1) {
-					$p->setfont('Helvetica-iso', 7);
-					$p->text((257/6*($monat-6*($page-1)-1)+59),
-						(($TOP-7)-($TOP-7-20)/31*($tag-1)-2.5), 
-						get_week_string($YEAR, $monat, $tag+1));
-					$p->setfont('Helvetica-iso', $FONTSIZE);
-				}
-					
-				# Tage mit Beschriftung (Sonntag, Termine, Feiertage)
-				if (Day_of_Week($YEAR, $monat, $tag) == 7 or
-					get_feiertag($tag, $monat) or get_termin($tag, $monat)) 
+
+                # Wochennummer am Monatsanfang
+                if ($tag == 1 and Day_of_Week($YEAR, $monat, $tag) == 1) {
+                    $p->setfont('Helvetica-iso', 7);
+                    $p->text((257/6*($monat-6*($page-1)-1)+59),
+                        (($TOP-7)-($TOP-7-20)/31*($tag-1)-2.5), 
+                        get_week_string($YEAR, $monat, $tag+1));
+                    $p->setfont('Helvetica-iso', $FONTSIZE);
+                }
+
+                # Tage mit Beschriftung (Sonntag, Termine, Feiertage)
+                if (Day_of_Week($YEAR, $monat, $tag) == 7 or
+                    get_feiertag($tag, $monat) or get_termin($tag, $monat)) 
                 {
-					# Termine
-					if (get_termin($tag, $monat)) 
+                    # Termine
+                    if (get_termin($tag, $monat)) 
                     {
-						$p->text(257/6*($monat-6*($page-1)-1)+32,
-							(($TOP-7)-($TOP-7-20)/31*$tag+1.5), 
-							get_termin($tag, $monat));
+                        $p->text(257/6*($monat-6*($page-1)-1)+32,
+                            (($TOP-7)-($TOP-7-20)/31*$tag+1.5), 
+                            get_termin($tag, $monat));
 
-					}
+                    }
 
-					# Sonntage
-					if (Day_of_Week($YEAR, $monat, $tag) == 7) 
+                    # Sonntage
+                    if (Day_of_Week($YEAR, $monat, $tag) == 7) 
                     {
-						# Abschlusslinie
-						$p->setlinewidth(0.5);
-						$p->line((257/6*($monat-6*($page-1)-1)+20),
-							(($TOP-7)-($TOP-7-20)/31*$tag),
-							257/6*($monat-6*($page-1))+20,
-							(($TOP-7)-($TOP-7-20)/31*$tag),);
+                        # Abschlusslinie
+                        $p->setlinewidth(0.5);
+                        $p->line((257/6*($monat-6*($page-1)-1)+20),
+                            (($TOP-7)-($TOP-7-20)/31*$tag),
+                            257/6*($monat-6*($page-1))+20,
+                            (($TOP-7)-($TOP-7-20)/31*$tag),);
 
-						# Wochennummer normal
-						if ($tag != Days_in_Month($YEAR, $monat)) 
+                        # Wochennummer normal
+                        if ($tag != Days_in_Month($YEAR, $monat)) 
                         {
-							$p->setfont('Helvetica-iso', 7);
-							$p->text((257/6*($monat-6*($page-1)-1)+59),
-								(($TOP-7)-($TOP-7-20)/31*$tag-2.5), 
-								get_week_string($YEAR, $monat, $tag+1));
-						}
-							
-						$p->setlinewidth(0.1);
-						$p->setfont('Helvetica-Bold-iso', $FONTSIZE);
-					}
+                            $p->setfont('Helvetica-iso', 7);
+                            $p->text((257/6*($monat-6*($page-1)-1)+59),
+                                (($TOP-7)-($TOP-7-20)/31*$tag-2.5), 
+                                get_week_string($YEAR, $monat, $tag+1));
+                        }
 
-					# Feiertag
-					if (get_feiertag($tag, $monat)) 
+                        $p->setlinewidth(0.1);
+                        $p->setfont('Helvetica-Bold-iso', $FONTSIZE);
+                    }
+
+                    # Feiertag
+                    if (get_feiertag($tag, $monat)) 
                     {
-						$p->setfont('Helvetica-iso', $FONTSIZE);
-						unless (get_termin($tag, $monat)) 
+                        $p->setfont('Helvetica-iso', $FONTSIZE);
+                        unless (get_termin($tag, $monat)) 
                         {
-							$p->text(257/6*($monat-6*($page-1)-1)+32,
-								(($TOP-7)-($TOP-7-20)/31*$tag+1.5), 
-								get_feiertag($tag, $monat));
-						}
-						$p->setfont('Helvetica-Bold-iso', $FONTSIZE);
-					}
+                            $p->text(257/6*($monat-6*($page-1)-1)+32,
+                                (($TOP-7)-($TOP-7-20)/31*$tag+1.5), 
+                                get_feiertag($tag, $monat));
+                        }
+                        $p->setfont('Helvetica-Bold-iso', $FONTSIZE);
+                    }
 
-				}
+                }
 
-				# Tagzahl
-				$p->text(257/6*($monat-6*($page-1)-1)+21.5+$zusatz, 
-					(($TOP-7)-($TOP-7-20)/31*$tag+1.5),
-					$tag);
+                # Tagzahl
+                $p->text(257/6*($monat-6*($page-1)-1)+21.5+$zusatz, 
+                    (($TOP-7)-($TOP-7-20)/31*$tag+1.5),
+                    $tag);
 
-				# Wochentag
-				$p->text(257/6*($monat-6*($page-1)-1)+25.5, 
-					(($TOP-7)-($TOP-7-20)/31*$tag+1.5), substr(Day_of_Week_Abbreviation(
-						Day_of_Week($YEAR, $monat, $tag)), 0, 2));
-			}
-		}
-	}
+                # Wochentag
+                $p->text(257/6*($monat-6*($page-1)-1)+25.5, 
+                    (($TOP-7)-($TOP-7-20)/31*$tag+1.5), substr(Day_of_Week_Abbreviation(
+                            Day_of_Week($YEAR, $monat, $tag)), 0, 2));
+            }
+        }
+    }
 }
 
 
 if ($DATEI eq "") 
 {
     # Drucken
-	if ($^O =~ /^Win/i)
+    if ($^O =~ /^Win/i)
     {
-		print STDERR "Drucken unter Windows wird nicht unterstützt.\n";
-		print STDERR "Erzeugen Sie eine Postscript-Datei und drucken sie mit Ghostscript aus.\n";
-	}
-	my $printer = defined $ENV{PRINTER} ? "-P$ENV{PRINTER}" : "";
+        print STDERR "Drucken unter Windows wird nicht unterstützt.\n";
+        print STDERR "Erzeugen Sie eine Postscript-Datei und drucken sie mit Ghostscript aus.\n";
+    }
+    my $printer = defined $ENV{PRINTER} ? "-P$ENV{PRINTER}" : "";
     $p->output("/tmp/kalenderdruck-$$.ps");
     system("cat /tmp/kalenderdruck-$$.ps | psselect -o -q | lpr $printer");
     print STDERR "ENTER drücken, um fortzufahren ...";
@@ -304,161 +304,160 @@ else
 
 sub get_week_string 
 {
-	my ($YEAR, $monat, $tag) = @_;
-	my $week;
+    my ($YEAR, $monat, $tag) = @_;
+    my $week;
 
-	$week = Week_Number($YEAR, $monat, $tag);
+    $week = Week_Number($YEAR, $monat, $tag);
 
-	$week = $week == 53 ? 1 : $week;
+    $week = $week == 53 ? 1 : $week;
 
-	return $week < 10 ? "  ".$week : "".$week;
+    return $week < 10 ? "  ".$week : "".$week;
 }
 
 sub get_feiertag 
 {
-	my ($tag, $monat) = @_;
+    my ($tag, $monat) = @_;
 
-	@normal_holidays = (
-		[  1,  1, "Neujahr" ],
-		[  1,  6, "Hl. 3 K\xF6nige" ],
-		[  5,  1, "Tag d. Arbeit"],
-		[  8, 15, "Mari\xE4 Himmelfahrt" ],
-		[ 11,  1, "Allerheiligen" ],
-		[ 12, 25, "1. Weihnachtsfeiert." ],
-		[ 12, 26, "2. Weihnachtsfeiert." ],
-	);
-	
-	# Nationalfeiertag
-	# 3. Oktober
-	if ($YEAR >= 1990) 
-    {
-		push @normal_holidays, [ 10, 3, "Tag der dt. Einheit" ];
-	}
-	# 17. Juni 1953 (bin nicht sicher, wann der eingeführt wurde)
-	if ($YEAR <= 1990 && $YEAR >= 1954) 
-    {
-		push @normal_holidays, [ 6, 17, "Nationalfeiertag" ];
-	}
+    @normal_holidays = (
+        [  1,  1, "Neujahr" ],
+        [  1,  6, "Hl. 3 K\xF6nige" ],
+        [  5,  1, "Tag d. Arbeit"],
+        [  8, 15, "Mari\xE4 Himmelfahrt" ],
+        [ 11,  1, "Allerheiligen" ],
+        [ 12, 25, "1. Weihnachtsfeiert." ],
+        [ 12, 26, "2. Weihnachtsfeiert." ],
+    );
 
-	for ($i = 0; $i < @normal_holidays; $i++) 
+    # Nationalfeiertag
+    # 3. Oktober
+    if ($YEAR >= 1990) 
     {
-		if ($normal_holidays[$i][0] == $monat &&
-			$normal_holidays[$i][1] == $tag) 
+        push @normal_holidays, [ 10, 3, "Tag der dt. Einheit" ];
+    }
+    # 17. Juni 1953 (bin nicht sicher, wann der eingeführt wurde)
+    if ($YEAR <= 1990 && $YEAR >= 1954) 
+    {
+        push @normal_holidays, [ 6, 17, "Nationalfeiertag" ];
+    }
+
+    for ($i = 0; $i < @normal_holidays; $i++) 
+    {
+        if ($normal_holidays[$i][0] == $monat &&
+            $normal_holidays[$i][1] == $tag) 
         {
-			return $HOLIDAYS ? $normal_holidays[$i][2] : " ";
-		}
-	}
+            return $HOLIDAYS ? $normal_holidays[$i][2] : " ";
+        }
+    }
 
 
-	# christl. Feiertage mit Bezug auf Ostern
-	# Berechnung aufgrund der Gauß'schen Osterformel (siehe Doku von Date::Calc)
+    # christl. Feiertage mit Bezug auf Ostern
+    # Berechnung aufgrund der Gauß'schen Osterformel (siehe Doku von Date::Calc)
 
-	my (undef, $em, $ed) = Easter_Sunday($YEAR);
-	
-		#
-		# Feiertage (aus der Dokumentation zu Date::Calc):
-		# Ostersonntag 	- 2 : Karfreitag
-		#				± 0 : Ostersonntag
-		#				+ 1 : Ostermontag
-		#				+39 : Christi Himmelfahrt ("Vatertag") (nicht bundeseinh.)
-		#				+49 : Pfingstsonntag
-		#				+50 : Pfingstmontag
-		#				+60 : Fronleichnam (nicht bundeseinheitl.)
+    my (undef, $em, $ed) = Easter_Sunday($YEAR);
 
-	my @namen = ("Karfreitag", "Ostersonntag", "Ostermontag", 
-		"Christi Himmelfahrt", "Pfingstsonntag", "Pfingstmontag", "Fronleichnam");
-	
-	$i = 0;
-	foreach (-2, 0, +1, +39, +49, +50, +60) 
+    #
+    # Feiertage (aus der Dokumentation zu Date::Calc):
+    # Ostersonntag  - 2 : Karfreitag
+    #               ± 0 : Ostersonntag
+    #               + 1 : Ostermontag
+    #               +39 : Christi Himmelfahrt ("Vatertag") (nicht bundeseinh.)
+    #               +49 : Pfingstsonntag
+    #               +50 : Pfingstmontag
+    #               +60 : Fronleichnam (nicht bundeseinheitl.)
+
+    my @namen = ("Karfreitag", "Ostersonntag", "Ostermontag", 
+        "Christi Himmelfahrt", "Pfingstsonntag", "Pfingstmontag", "Fronleichnam");
+
+    $i = 0;
+    foreach (-2, 0, +1, +39, +49, +50, +60) 
     {
-		if ((Add_Delta_Days($YEAR, $em, $ed, $_))[1] == $monat &&
-			(Add_Delta_Days($YEAR, $em, $ed, $_))[2] == $tag) 
+        if ((Add_Delta_Days($YEAR, $em, $ed, $_))[1] == $monat &&
+            (Add_Delta_Days($YEAR, $em, $ed, $_))[2] == $tag) 
         {
-			return $HOLIDAYS ? $namen[$i] : " ";
-		}
-		$i++;
-	}
-	
-	return 0;
+            return $HOLIDAYS ? $namen[$i] : " ";
+        }
+        $i++;
+    }
+
+    return 0;
 }
 
 {
-	my @termine;	# statische Variable in C/C++
-	my $termine_read = 0;
+    my @termine;	# statische Variable in C/C++
+    my $termine_read = 0;
 
-	sub parse_termine 
+    sub parse_termine 
     {
 
-		if ($TERMINFILE eq "") 
+        if ($TERMINFILE eq "") 
         {
-			return;
-		}
+            return;
+        }
 
-		open (TERMINE, "<$TERMINFILE") or die "Die Termindatei $TERMINFILE konnte ".
-			"nicht geöffnet werden: $!";
+        open (TERMINE, "<$TERMINFILE") or die "Die Termindatei $TERMINFILE konnte ".
+            "nicht geöffnet werden: $!";
 
-		while (<TERMINE>) 
+        while (<TERMINE>) 
         {
-			if (/^#/ or /^\s+$/) 
+            if (/^#/ or /^\s+$/) 
             {
-				next;
-			}
+                next;
+            }
             else 
             {
-				my ($tag, $monat, $jahr, $bezeichnung);
-				if (/^\d{2}\.\d{2}\.\d{4}\s+/) 
+                my ($tag, $monat, $jahr, $bezeichnung);
+                if (/^\d{2}\.\d{2}\.\d{4}\s+/) 
                 {
                     ($tag, $monat, $jahr, $bezeichnung) = 
                             /^(\d{2})\.(\d{2})\.(\d{4})\s+(.*)$/;
-					if ($jahr != $YEAR) 
+                    if ($jahr != $YEAR) 
                     {
                         next;
-					}
-				}
+                    }
+                }
                 elsif (/^\d{2}\.\d{2}\.\s+/) 
                 {
-					($tag, $monat, $bezeichnung) = /^(\d{2})\.(\d{2})\.\s+(.*)$/;
-				}
+                    ($tag, $monat, $bezeichnung) = /^(\d{2})\.(\d{2})\.\s+(.*)$/;
+                }
                 else 
                 {
-					next;
-				}
-				$bezeichnung =~ s/-/­/g;	# kurze Bindestriche
-				push @termine, [ $monat, $tag, $bezeichnung ];
-			}
-		}
-        
-		close TERMINE or die "Die Termindatei $TERMINFILE konnte nicht ".
-			"geschlossen werden: $!";
-	}
-    
-	sub get_termin
+                    next;
+                }
+                $bezeichnung =~ s/-/­/g;    # kurze Bindestriche
+                push @termine, [ $monat, $tag, $bezeichnung ];
+            }
+        }
+
+        close TERMINE or die "Die Termindatei $TERMINFILE konnte nicht ".
+              "geschlossen werden: $!";
+    }
+
+    sub get_termin
     {
-        
-		my ($tag, $monat) = @_;
-        
-		if ($TERMINFILE eq "") 
+        my ($tag, $monat) = @_;
+
+        if ($TERMINFILE eq "") 
         { 
-			return 0;
-		}
-        
-		if (!$termine_read)
+            return 0;
+        }
+
+        if (!$termine_read)
         {
-			parse_termine();
-			$termine_read = 1;
-		}
-        
-		for ($i = 0; $i < @termine; $i++) 
+            parse_termine();
+            $termine_read = 1;
+        }
+
+        for ($i = 0; $i < @termine; $i++) 
         {
-			if ($termine[$i][0] == $monat &&
-					$termine[$i][1] == $tag) 
+            if ($termine[$i][0] == $monat &&
+                $termine[$i][1] == $tag) 
             {
-				return $termine[$i][2];
-			}
-		}
-        
-		return 0;
-	}
+                return $termine[$i][2];
+            }
+        }
+
+        return 0;
+    }
 }
 
 # vim: set sw=4 ts=4 noet:
